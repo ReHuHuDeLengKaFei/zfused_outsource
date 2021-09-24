f4d7ba13a4ceac23ac9fadea536f22266053d20c
zfused <zhoulh0322@gmail.com>
2021-09-24 11:15:37 +0800

    fixs

--- packages/zwidgets/assetwidget/versionlistwidget/constants.py
+++ packages/zwidgets/assetwidget/versionlistwidget/constants.py
@@ -3,10 +3,21 @@
 
 __all__ = ["Constants"]
 
-class Constants(object):
-    ITEM_DELEGATE_SIZE = [160,140]
+class _Constants(object):
+    ITEM_DELEGATE_SIZE = [160,80]
+    THUMBNAIL_WIDTH = 160
     THUMBNAIL_HEIGHT = 80
+
     INFOMATION_HEIGHT = 60
+    BACKGROUND_COLOR = "#F0F0F0"
+    NAME_BACKGROUND_COLOR = "#F0F0F0"
+    NAME_COLOR = "#555555"
+    FONT_SIZE = 11
+
+class Constants(object):
+    SPACING = 10
+    ITEM_DELEGATE_SIZE = [160, 36]
+    THUMBNAIL_SIZE = [64, 36]
     BACKGROUND_COLOR = "#F0F0F0"
     NAME_BACKGROUND_COLOR = "#F0F0F0"
     NAME_COLOR = "#555555"
